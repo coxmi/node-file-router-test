1b8384f72e3924a366f1b5cc05a4bdabc3759969
Daniil Sitdikov <hello.boriskas@gmail.com>
2023-11-01 18:43:25 +0100

    add different way of watch mode

--- server.ts
+++ server.ts
@@ -1,42 +1,17 @@
-
 import express from 'express'
 import { initFileRouter } from 'node-file-router'
 
 const port = process.env.PORT || 4321
-const fileRouter: { handler?: (req, res) => any } = {}
 
-
-export async function setupRouter() {
-	console.log('setupRouter')
-	delete fileRouter.handler
-	fileRouter.handler = await initFileRouter({
+export async function serve() {
+	const app = express()
+	const useFileRouter = await initFileRouter({
 		baseDir: `${__dirname}/routes`,
 	})
-}
 
+	app.use(useFileRouter);
 
-export async function serve({ dev }: { dev?: boolean } = {}) {
-	
-
-	const app = express()
-	await setupRouter()
-
-	app.use(async (req, res) => {
-		console.log(`———— load page: ${req.url} ————`)
-
-		if (dev) {
-			console.log('clear modules')
-			await setupRouter()
-			const paths = Object.keys(require.cache)
-			const frontend = paths.filter(x => x.includes('.build/routes'))
-			frontend.map(path => {
-				delete require.cache[path]
-			})
-		}
-		return fileRouter.handler(req, res)
-	})
-
-	app.listen(port)	
+	app.listen(port)
 	console.log(`Serving on http://localhost:${port}`)
 }
 
